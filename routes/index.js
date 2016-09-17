bbadb539785fc44109e29fef0f49152da83a541c
chen.zhang <chen.zhang@alibaba-inc.com>
2016-09-17 14:13:38 +0800

    deploy mongodb link ~

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+//mongodb
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("hello !!");
+
+  var url =  'mongodb://heroku_gkzcbvpx:1q2w3e@ds033106.mlab.com:33106/heroku_gkzcbvpx';
+
+  MongoClient.connect(url, function (err,db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+  })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+//mongodb
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+
+
+  var url =  'mongodb://main_access:123456@ds033106.mlab.com:33106/heroku_gkzcbvpx';
+
+  MongoClient.connect(url, function (err,db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+  })
 });
 
 module.exports = router;
